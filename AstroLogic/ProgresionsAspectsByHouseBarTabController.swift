e5e380210a3130799778c24fed73a968edb2f547
Errick Miron <highsman@lilaverse.app>
2023-10-06 21:15:10 -0700

    added selectedDate to ProgresedAspectsByHouse

--- AstroLogic/ProgresionsAspectsByHouseBarTabController.swift
+++ AstroLogic/ProgresionsAspectsByHouseBarTabController.swift
@@ -21,19 +21,19 @@ class ProgressionsAspectsByHouseTabBarController: UITabBarController {
 
 
         let progressedAspects = SimpleProgressedAspectsByHousesVC()
-        progressedAspects.selectedDate = Date()
+        progressedAspects.selectedDate = selectedDate
         progressedAspects.chartCake = chartCake
         progressedAspects.tabBarItem = UITabBarItem(title: "Majors", image: UIImage(systemName: "sun.max.fill"), tag: 0)
 
         // For Minors (moon symbol is just an example; you may need to find a suitable SF Symbol)
         let minorProgressedAspects = SimpleMinorProgressedAspectsByHousesVC()
-        minorProgressedAspects.selectedDate = Date()
+        minorProgressedAspects.selectedDate = selectedDate
         minorProgressedAspects.chartCake = chartCake
         minorProgressedAspects.tabBarItem = UITabBarItem(title: "Minors", image: UIImage(systemName: "moon.fill"), tag: 1)
 
         // For Transits (earth symbol is just an example; you may need to find a suitable SF Symbol)
         let transitAspects = SimpleTransitAspectsByHousesVC()
-        transitAspects.selectedDate = Date()
+        transitAspects.selectedDate = selectedDate
         transitAspects.chartCake = chartCake
         transitAspects.tabBarItem = UITabBarItem(title: "Transits", image: UIImage(systemName: "globe"), tag: 2)
 
