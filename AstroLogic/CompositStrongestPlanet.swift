8393dade480bac15820504877cf1c7e39de5cd2f
Errick Miron <highsman@lilaverse.app>
2023-11-09 10:43:29 -0800

    began working on strongestCompositePlanet

---
+++ AstroLogic/CompositStrongestPlanet.swift
@@ -0,0 +1,8 @@
+//
+//  CompositStrongestPlanet.swift
+//  AstroLogic
+//
+//  Created by Errick Williams on 11/8/23.
+//
+
+import Foundation
---
+++ AstroLogic/MyCompositeItemsVC.swift
@@ -0,0 +1,8 @@
+//
+//  MyCompositeItemsVC.swift
+//  AstroLogic
+//
+//  Created by Errick Williams on 11/8/23.
+//
+
+import Foundation
