5054409f50d099fd437e851da9a6a632a635813f
Errick Miron <highsman@lilaverse.app>
2023-11-20 00:09:16 -0800

    added contentDaya

---
+++ AstroLogic/Progressions DATA.swift
@@ -0,0 +1,24 @@
+//
+//  Progressions DATA.swift
+//  AstroLogic
+//
+//  Created by Errick Williams on 11/19/23.
+//
+
+import Foundation
+struct PlanetaryAspectContent {
+    var movingPlanetContent1: String
+    var movingPlanetContent2: String
+    var movingPlanetContent3: String
+    var movingPlanetContent4: String
+    var movingPlanetContent5: String
+}
+
+
+let mercuryContactingSaturn = PlanetaryAspectContent(
+    movingPlanetContent1: "What I Need to Learn: That this is a time in my life...",
+    movingPlanetContent2: "Potential Blindspot: Any hesitation I might feel...",
+    movingPlanetContent3: "Omens and Synchronicities: Situations arise indicating...",
+    movingPlanetContent4: "How to Recognize Your Teachers: They are serious people...",
+    movingPlanetContent5: "Squandered: Missing a real chance to arm myself..."
+)
