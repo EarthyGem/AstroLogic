f0ada638e261a24a16b9af0092a173d273ee5f8b
Errick Miron <highsman@lilaverse.app>
2024-01-06 20:51:23 -0800

    Revert "changes"
    This reverts commit 41cb9fb4729e85cd70fbff26fbb6ddfcc8c6044a.

--- AstroLogic/RelationshipItemsVC.swift
+++ AstroLogic/RelationshipItemsVC.swift
@@ -49,28 +49,26 @@ class RelationshipItemsViewController: UIViewController {
             RelationshipItems(chartType: "\(String(describing: name!))'s Synastry Chart with \(String(describing: otherName!))"), RelationshipItems(chartType: "Interaspects between \(String(describing: name!)) and \(String(describing: otherName!))"),
             RelationshipItems(chartType: "\(String(describing: name!)) and \(String(describing: otherName!))'s Transposed Houses"),
             RelationshipItems(chartType: "\(String(describing: name!)) and \(String(describing: otherName!))'s Relationship Chart"),
-       //     RelationshipItems(chartType: "\(selectedName ?? "")'s Natal Houses"),
-       //     RelationshipItems(chartType: "\(selectedName ?? "")'s Natal Aspects"),
-       //     RelationshipItems(chartType: "\(selectedName ?? "")'s Transit Progressions"),
-      //      RelationshipItems(chartType: "\(selectedName ?? "")'s Transit Aspects"),
-      //      RelationshipItems(chartType: "\(selectedName ?? "")'s Minor Progressions"),
-      //      RelationshipItems(chartType: "\(selectedName ?? "")'s Minor Progression Aspects"),
-     //       RelationshipItems(chartType: "\(selectedName ?? "")'s Major Progressions")
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Natal Houses"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Natal Aspects"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Transit Progressions"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Transit Aspects"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Minor Progressions"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Minor Progression Aspects"),
+            //     RelationshipItems(chartType: "\(selectedName ?? "")'s Major Progressions")
 
         ]
 
         tableView.reloadData()
-        }
-
-        override func viewDidLayoutSubviews() {
-            super.viewDidLayoutSubviews()
-            tableView.frame = view.bounds
-
-
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
+
+}
+
 extension RelationshipItemsViewController: AddRelationshipDelegate {
     func addRelationship(_ relationship: Relationships, chart: ChartCake, chartCake: ChartCake) {
         let newRelationship = RelationshipItems(chartType: relationship.name)
@@ -91,76 +89,69 @@ extension RelationshipItemsViewController: AddRelationshipDelegate {
     }
 }
 
+extension RelationshipItemsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+
+        let OthersPlanetsVC = OthersPlanetsViewController(planets: [""])
+        OthersPlanetsVC.chartCake = self.chartCake
+        OthersPlanetsVC.otherChart = self.otherChart
+        OthersPlanetsVC.name = self.name
+        OthersPlanetsVC.selectedName = self.selectedName
+
+
+       let SynastryChartVC = SynastryViewController(MP_Planets: [""])
+        SynastryChartVC.chartCake = self.chartCake
+        SynastryChartVC.otherChart = self.otherChart
+        SynastryChartVC.name = self.name
+        SynastryChartVC.selectedName = self.selectedName
+
+
+        let transposedHousesVC = TransposedHousesVC()
+        transposedHousesVC.chartCake = self.chartCake
+        transposedHousesVC.otherChart = self.otherChart
+        transposedHousesVC.title = "\(String(describing: name!))'s Houses"
+        transposedHousesVC.name = self.name
+        transposedHousesVC.selectedName = self.otherName
+        let natalPlanetsVC = PlanetsViewController(planets: placeHolder)
+
+        let interAspectsVC = SimpleInteraspectsViewController()
+        interAspectsVC.chartCake = chartCake
+        interAspectsVC.otherChart = otherChart
+        interAspectsVC.name = name
+        interAspectsVC.otherName = otherName
 
 
 
-        extension RelationshipItemsViewController: UITableViewDelegate {
-            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                tableView.deselectRow(at: indexPath, animated: true)
+
+        let compositeVC = MyCompositeItemsViewController()
+        let chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
+        compositeVC.chartCake = chartCake
+        compositeVC.otherChart = otherChart
+        compositeVC.houseScores = self.chartCake!.calculateHouseStrengths()
+        compositeVC.chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
+      //  compositeVC.harmonyDiscordScores = getScoresAndDifferenceForPlanets(chart: Chart(alpha: chartCake!.natal, bravo: otherChart!.natal))
+        compositeVC.scores2 = getTotalPowerScoresForPlanets(chart: chart)
+        compositeVC.scores = getTotalPowerScoresForPlanets2(chart: chart)
+        compositeVC.chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
 
 
-                let OthersPlanetsVC = OthersPlanetsViewController(planets: [""])
-                OthersPlanetsVC.chartCake = self.chartCake
-                OthersPlanetsVC.otherChart = self.otherChart
-                OthersPlanetsVC.name = self.name
-                OthersPlanetsVC.selectedName = self.selectedName
-
- 
-               let SynastryChartVC = SynastryViewController(MP_Planets: [""])
-                SynastryChartVC.chartCake = self.chartCake
-                SynastryChartVC.otherChart = self.otherChart
-                SynastryChartVC.name = self.name
-                SynastryChartVC.selectedName = self.selectedName
+        let categories = [OthersPlanetsVC,SynastryChartVC,interAspectsVC,transposedHousesVC,compositeVC]
+        navigationController?.pushViewController(categories[indexPath.row] , animated: true)
+    }
 
 
-                let transposedHousesVC = TransposedHousesVC()
-                transposedHousesVC.chartCake = self.chartCake
-                transposedHousesVC.otherChart = self.otherChart
-                transposedHousesVC.title = "\(String(describing: name!))'s Houses"
-                transposedHousesVC.name = self.name
-                transposedHousesVC.selectedName = self.otherName
-                let natalPlanetsVC = PlanetsViewController(planets: placeHolder)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return relationshipData.count
+    }
 
-                let interAspectsVC = SimpleInteraspectsViewController()
-                interAspectsVC.chartCake = chartCake
-                interAspectsVC.otherChart = otherChart
-                interAspectsVC.name = name
-                interAspectsVC.otherName = otherName
-          
-              
-
-
-                let compositeVC = MyCompositeItemsViewController()
-                let chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
-                compositeVC.chartCake = chartCake
-                compositeVC.otherChart = otherChart
-                compositeVC.houseScores = self.chartCake!.calculateHouseStrengths()
-                compositeVC.chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
-              //  compositeVC.harmonyDiscordScores = getScoresAndDifferenceForPlanets(chart: Chart(alpha: chartCake!.natal, bravo: otherChart!.natal))
-                compositeVC.scores2 = getTotalPowerScoresForPlanets(chart: chart)
-                compositeVC.scores = getTotalPowerScoresForPlanets2(chart: chart)
-                compositeVC.chart = Chart(alpha: chartCake!.natal, bravo: otherChart!.natal)
-
-
-                let categories = [OthersPlanetsVC,SynastryChartVC,interAspectsVC,transposedHousesVC,compositeVC]
-                navigationController?.pushViewController(categories[indexPath.row] , animated: true)
-
-
-            }
-        }
-
-
-        extension RelationshipItemsViewController: UITableViewDataSource {
-            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                return relationshipData.count
-            }
-
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-                cell.textLabel?.text = relationshipData[indexPath.row].chartType
-                return cell
-            }
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = relationshipData[indexPath.row].chartType
+        return cell
+    }
+}
 
 
         
